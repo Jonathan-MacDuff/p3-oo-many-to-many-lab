a73239180f3ebb47a064478073e30ab82c70ffc4
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-04-27 16:07:50 -0700

    completed lab

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,90 @@
 class Author:
-    pass
+
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        contract_list = []
+        for contract in Contract.all:
+            if contract.author == self:
+                contract_list.append(contract)
+        return contract_list
+    
+    def books(self):
+        book_list = []
+        for contract in Contract.all:
+            if contract.author == self:
+                book_list.append(contract.book)
+        return book_list
+    
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        return contract
+    
+    def total_royalties(self):
+        royalties_total = 0
+        for contract in Contract.all:
+            if contract.author == self:
+                royalties_total += contract.royalties
+
+        return royalties_total
 
 
 class Book:
-    pass
 
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        contract_list = []
+        for contract in Contract.all:
+            if contract.book == self:
+                contract_list.append(contract)
+        return contract_list
+    
+    def authors(self):
+        author_list = []
+        for contract in Contract.all:
+            if contract.book == self:
+                author_list.append(contract.author)
+        return author_list
+
+
+    
 
 class Contract:
-    pass
+
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if isinstance (author, Author):
+            self.author = author
+        else:
+            raise TypeError("Author must be an instance of Author class.")
+        if isinstance (book, Book):
+            self.book = book
+        else:
+            raise TypeError("Book must be an instance of Book class.")
+        if isinstance(date, str):
+            self.date = date
+        else: 
+            raise TypeError("Date must be a string.")
+        if isinstance(royalties, int):
+            self.royalties = royalties
+        else:
+            raise TypeError("Royalties must be an integer.")
+        Contract.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        contract_list = []
+        for contract in Contract.all:
+            if contract.date == date:
+                contract_list.append(contract)
+        return contract_list
